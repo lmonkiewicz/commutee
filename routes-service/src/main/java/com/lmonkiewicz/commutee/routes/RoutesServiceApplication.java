40f8ba97c88b50617a678e33ffb6945c14238e11
lmonkiewicz <lmonkiewicz@gmail.com>
2017-02-18 21:57:25 +0100

    Created spring boot app

---
+++ routes-service/src/main/java/com/lmonkiewicz/commutee/routes/RoutesServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lmonkiewicz.commutee.routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutesServiceApplication.class, args);
+	}
+}
