c94231eda79195bb63487cfe90d0c06cc820398e
Łukasz Monkiewicz <lukasz.monkiewicz@primaris.eu>
2017-02-22 23:13:36 +0100

    Enforced encoding in input stream of test data

--- routes-service/src/test/java/com/lmonkiewicz/commutee/routes/parser/warsaw/BaseSectionReaderTest.java
+++ routes-service/src/test/java/com/lmonkiewicz/commutee/routes/parser/warsaw/BaseSectionReaderTest.java
@@ -2,12 +2,14 @@ package com.lmonkiewicz.commutee.routes.parser.warsaw;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by lmonkiewicz on 2017-02-20.
  */
 public class BaseSectionReaderTest {
-    protected BufferedReader createReader(String fileName){
-        return new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(fileName)));
+    protected BufferedReader createReader(String fileName) throws UnsupportedEncodingException {
+        return new BufferedReader(
+                new InputStreamReader(this.getClass().getResourceAsStream(fileName), "UTF-8"));
     }
 }
