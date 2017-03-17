d9ac5879b4fbc2b2e2ff0e39f9199f43ed774f57
Łukasz Monkiewicz <lukasz.monkiewicz@atman.pl>
2017-03-17 10:30:17 +0100

    Added test for combined section parsing

---
+++ routes-service/src/test/java/com/lmonkiewicz/commutee/routes/parser/warsaw/section/RootSectionReaderTest.java
@@ -0,0 +1,36 @@
+package com.lmonkiewicz.commutee.routes.parser.warsaw.section;
+
+import com.lmonkiewicz.commutee.routes.parser.warsaw.BaseSectionReaderTest;
+import com.lmonkiewicz.commutee.routes.parser.warsaw.model.ZtmData;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * Created by lmonkiewicz on 17.03.2017.
+ */
+public class RootSectionReaderTest extends BaseSectionReaderTest {
+
+    @Test
+    public void correctlyParsesWholeFileStructure() throws Exception {
+        try(BufferedReader in = createReader("RootSectionData.txt")){
+            RootSectionReader reader = new RootSectionReader();
+            reader.readSection(in);
+
+            final ZtmData ztmData = reader.result();
+
+            assertThat(ztmData).isNotNull();
+            assertThat(ztmData.getDayTypes()).isNotNull();
+            assertThat(ztmData.getRoutesCalendar()).isNotNull();
+            assertThat(ztmData.getLinesCalendar()).isNotNull();
+            assertThat(ztmData.getBusStopGroupsList()).isNotNull();
+            assertThat(ztmData.getBusStopGroups()).isNotNull();
+            assertThat(ztmData.getTownCodes()).isNotNull();
+            assertThat(ztmData.getLines()).isNotNull();
+        }
+
+    }
+}
